e307e7694d3bb392e6bbcc72f9b56ce2b01d9488
TusharKaundal <kaundaltushar@gmail.com>
2021-09-29 12:20:18 +0530

    intialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCH4Vc4ybBQdFK8XGV25Qu9CmA5S284glI',
+  authDomain: 'chat-web-app-acdc9.firebaseapp.com',
+  databaseURL:
+    'https://chat-web-app-acdc9-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'chat-web-app-acdc9',
+  storageBucket: 'chat-web-app-acdc9.appspot.com',
+  messagingSenderId: '293156563229',
+  appId: '1:293156563229:web:7c056f2375443e542f99f4',
+};
+
+const app = firebase.initializeApp(firebaseConfig);
