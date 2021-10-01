c99980d40fc6ae2403340d76d92bd3c9d21a217c
TusharKaundal <kaundaltushar@gmail.com>
2021-10-01 11:41:54 +0530

    added  rsuit default css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React from 'react';
+import 'rsuite/dist/rsuite.min.css';
+import './styles/main.scss';
 
 function App() {
   return <div className="App">hi</div>;
