f8461b43504d56c480b5a1c0f945dd398672d791
TusharKaundal <kaundaltushar@gmail.com>
2021-10-21 13:41:52 +0530

    realtime presence listener

--- src/components/dashboard/DashBoardToggle.js
+++ src/components/dashboard/DashBoardToggle.js
@@ -1,21 +1,33 @@
 import Dashboard from '@rsuite/icons/Dashboard';
 import React, { useCallback } from 'react';
 import { Button, Drawer, toaster, Message } from 'rsuite';
+import { ref, set } from '@firebase/database';
 import DashBoard from '.';
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
-import { auth } from '../../misc/firebase';
+import { auth, db } from '../../misc/firebase';
+import { isOfflineForDatabase } from '../../context/profile.context';
 
 const DashBoardToggle = () => {
   const { isOpen, open, close } = useModalState();
   const isMobile = useMediaQuery('(max-width:992px)');
   const onSignOut = useCallback(() => {
-    auth.signOut();
-    toaster.push(
-      <Message showIcon type="info" duration={2000}>
-        Signed Out
-      </Message>
-    );
-    close();
+    set(ref(db, `/status/${auth.currentUser.uid}`), isOfflineForDatabase)
+      .then(() => {
+        auth.signOut();
+        toaster.push(
+          <Message showIcon type="info" duration={2000}>
+            Signed Out
+          </Message>
+        );
+        close();
+      })
+      .catch(err => {
+        toaster.push(
+          <Message showIcon type="error" duration={2000}>
+            {err.message}
+          </Message>
+        );
+      });
   }, [close]);
   return (
     <>
--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,8 +1,25 @@
 /* eslint-disable no-unused-vars */
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { onAuthStateChanged } from 'firebase/auth';
-import { ref, onValue, off } from 'firebase/database';
+import {
+  ref,
+  onValue,
+  off,
+  serverTimestamp,
+  onDisconnect,
+  set,
+} from 'firebase/database';
 import { auth, db } from '../misc/firebase';
+
+export const isOfflineForDatabase = {
+  state: 'offline',
+  last_changed: serverTimestamp(),
+};
+
+const isOnlineForDatabase = {
+  state: 'online',
+  last_changed: serverTimestamp(),
+};
 
 const ProfileContext = createContext();
 
@@ -11,8 +28,10 @@ export const ProfileProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     let userRef;
+    let userStatusRef;
     const authUnsub = onAuthStateChanged(auth, authObj => {
       if (authObj) {
+        userStatusRef = ref(db, `/status/${authObj.uid}`);
         userRef = ref(db, `/profiles/${authObj.uid}`);
         onValue(userRef, snap => {
           const { username, createdAt, avatar } = snap.val();
@@ -27,9 +46,24 @@ export const ProfileProvider = ({ children }) => {
           setProfile(data);
           setIsLoading(false);
         });
+
+        onValue(ref(db, '.info/connected'), snapshot => {
+          // If we're not currently connected, don't do anything.
+          if (snapshot.val() === false) {
+            return;
+          }
+          // creating presence in realtime database
+          set(onDisconnect(userStatusRef), isOfflineForDatabase).then(() => {
+            set(userStatusRef, isOnlineForDatabase);
+          });
+        });
       } else {
         if (userRef) {
           off(userRef);
+        }
+        off(ref(db, '.info/connected'));
+        if (userStatusRef) {
+          off(userStatusRef);
         }
         setProfile(null);
         setIsLoading(false);
@@ -39,6 +73,11 @@ export const ProfileProvider = ({ children }) => {
       authUnsub();
       if (userRef) {
         off(userRef);
+      }
+      off(ref(db, '.info/connected'));
+
+      if (userStatusRef) {
+        off(userStatusRef);
       }
     };
   }, []);
